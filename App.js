e21cc176aa3583d16c786749a7a8314fd23272e2
Mushroms <macbookair@MacbookAirs-MacBook-Air.local>
2020-02-12 14:12:12 +0300

    firs commit

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Papers from './components/papers';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Papers />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ components/papers.js
@@ -0,0 +1,63 @@
+import type {Node} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const links = [
+  {
+    title: 'Example Paper',
+    link: 'https://facebook.github.io/react-native/docs/style',
+    description: 'Example',
+  },
+];
+
+const papers = (): Node => (
+  <View style={styles.container}>
+    {links.map((item, index) => {
+      return (
+        <React.Fragment key={index}>
+          <View style={styles.separator} />
+          <TouchableOpacity
+            accessibilityRole={'button'}
+            //onPress={() => }
+            style={styles.linkContainer}>
+            <Text style={styles.link}>{item.title}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+          </TouchableOpacity>
+        </React.Fragment>
+      );
+    })}
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  linkContainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  link: {
+    flex: 2,
+    fontSize: 18,
+    fontWeight: '400',
+    //color: Colors.primary,
+  },
+  description: {
+    flex: 3,
+    paddingVertical: 16,
+    fontWeight: '400',
+    fontSize: 18,
+    //color: Colors.dark,
+  },
+  separator: {
+    //backgroundColor: Colors.light,
+    height: 1,
+  },
+});
+
+export default papers;
