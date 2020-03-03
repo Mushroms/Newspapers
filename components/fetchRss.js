b19640da63e2308d13366b5dc0b5a81d80b5908f
Mushroms <macbookair@MacbookAirs-MacBook-Air.local>
2020-03-03 20:27:35 +0300

    parse xml to json

--- components/fetchRss.js
+++ components/fetchRss.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {parseString} from 'react-native-xml2js';
 
 export default class FetchRss extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      rss: [],
+      rss: {},
     };
   }
 
   getData() {
     return fetch('https://www.vesti.ru/vesti.rss')
       .then(response => response.text())
-      .then(responseData => {
-        console.log(responseData);
-        this.setState({rss: responseData});
+      .then(responseDataXml => {
+        //console.log(responseDataXml);
+        parseString(responseDataXml, (err, result) => {
+          //console.log('Channel information:', result.rss.channel);
+          //console.log('List all news:', result.rss.channel[0].item);
+          this.setState({rss: result.rss.channel[0]});
+        });
       })
       .catch(error => {
         console.error(error);
@@ -25,6 +30,6 @@ export default class FetchRss extends React.Component {
     this.getData();
   }
   render() {
-    return <Text>{this.state.rss}</Text>;
+    return <Text>{this.state.rss.title}</Text>;
   }
 }
